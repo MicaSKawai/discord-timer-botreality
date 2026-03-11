77677e186a1f7781a0052f6b73d3cbc1060426b1
MicaSKawai <alexslachta@gmail.com>
2026-03-11 05:20:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import os
 import discord
 from discord.ext import commands
+import os
 
 TOKEN = os.getenv("DISCORD_TOKEN")
-
-if TOKEN is None:
-    raise Exception("ERROR: No se encontró la variable DISCORD_TOKEN en Railway")
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -14,10 +11,10 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f"✅ Bot conectado como {bot.user}")
+    print(f"Bot conectado como {bot.user}")
 
 @bot.command()
 async def ping(ctx):
-    await ctx.send("🏓 Pong! El bot funciona.")
+    await ctx.send("Pong! Bot funcionando")
 
 bot.run(TOKEN)
