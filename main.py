9634da6df93156ecb276c31c35bc877a3228ba6a
MicaSKawai <alexslachta@gmail.com>
2026-03-12 18:14:41 -0300

    Allow SQLite connection to be used across threads

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 
 # ---------------- DATABASE ----------------
 
-db = sqlite3.connect("timers.db")
+db = sqlite3.connect("timers.db", check_same_thread=False)
 cursor = db.cursor()
 
 cursor.execute("""
